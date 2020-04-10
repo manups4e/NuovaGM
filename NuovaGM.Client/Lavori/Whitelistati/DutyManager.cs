4d06f7d6c09714513d375ca2022a3863d178e94e
manups4e <manups4e@gmail.com>
2020-04-10 10:39:53 +0200

    commit obbligatorio

--- NuovaGM.Client/Lavori/Whitelistati/DutyManager.cs
+++ NuovaGM.Client/Lavori/Whitelistati/DutyManager.cs
@@ -12,16 +12,7 @@ namespace NuovaGM.Client.Lavori.Whitelistati
 		private static bool Medici = false;
 		public static void Init()
 		{
-			Client.GetInstance.RegisterEventHandler("lprp:onPlayerSpawn", new Action(Spawnato));
-		}
-
-		private static async void Spawnato()
-		{
-			Client.GetInstance.RegisterTickHandler(ControlloLavori);
-		}
-
-		public static async Task ControlloLavori()
-		{
+//			Client.GetInstance.RegisterEventHandler("lprp:onPlayerSpawn", new Action(Spawnato));
 		}
 	}
 }
