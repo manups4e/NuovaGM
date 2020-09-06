9ac01595ecd1d3f400c50cef9e52169d08d09bbe
manups4e <manups4e@gmail.com>
2020-09-06 10:22:21 +0200

    agg. coord. telecamera

--- NuovaGM.Client/Manager/DevManager.cs
+++ NuovaGM.Client/Manager/DevManager.cs
@@ -43,11 +43,11 @@ namespace NuovaGM.Client.Manager
 		public static async Task OnTickSviluppo()
 		{
 
-			HUD.DrawText(0.4f, 0.925f, $"Posizione: {Game.PlayerPed.Position}");
+			HUD.DrawText(0.4f, 0.925f, $"~o~Posizione~w~: {Game.PlayerPed.Position}");
 			HUD.DrawText(0.4f, 0.95f, $"Rotazione: {GetEntityRotation(PlayerPedId(), 2)}");
 			HUD.DrawText(0.4f, 0.90f, $"Interior Id = {GetInteriorFromGameplayCam()}");
-			HUD.DrawText(0.7f, 0.90f, $"GamePlayCam punta a = {Funzioni._CrosshairRaycast().HitPosition}");
-			HUD.DrawText(0.7f, 0.925f, $"GamePlayCam Posizione = {GameplayCamera.Position}");
+			HUD.DrawText(0.7f, 0.925f, $"~b~GamePlayCam Posizione~w~ = {GameplayCamera.Position}");
+			HUD.DrawText(0.7f, 0.90f, $"~r~GamePlayCam punta a~w~ = {Funzioni._CrosshairRaycast().HitPosition}");
 			if (Game.PlayerPed.IsAiming)
 			{
 				int entity = 0;
