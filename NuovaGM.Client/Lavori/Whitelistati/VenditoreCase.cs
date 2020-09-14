b26f30faf0fa0c58bac8e260f8493e750c828930
manups4e <manups4e@gmail.com>
2020-09-14 11:19:35 +0200

    Iniziati Venditori di case e Garage

---
+++ NuovaGM.Client/Lavori/Whitelistati/VenditoreCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuovaGM.Client.Lavori.Whitelistati
+{
+	static class VenditoreCase
+	{
+		public static void Init()
+		{
+			
+		}
+	}
+}
---
+++ NuovaGM.Client/Proprietà/Garage/GarageClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuovaGM.Client.Proprietà.Garage
+{
+	static class GarageClient
+	{
+		public static void Init()
+		{
+
+		}
+	}
+}
