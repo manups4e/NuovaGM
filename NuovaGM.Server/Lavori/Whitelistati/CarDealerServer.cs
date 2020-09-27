5e3174ceb4866469dc76ddadf34242dde6b5f312
manups4e <manups4e@gmail.com>
2020-09-27 19:03:40 +0200

    cambia veicolo del catalogo per tutti in anteprima

--- NuovaGM.Server/Lavori/Whitelistati/CarDealerServer.cs
+++ NuovaGM.Server/Lavori/Whitelistati/CarDealerServer.cs
@@ -11,12 +11,18 @@ namespace NuovaGM.Server.Lavori.Whitelistati
 		public static void Init()
 		{
 			Server.Instance.AddEventHandler("lprp:cardealer:attivaCatalogoAlcuni", new Action<Player, List<int>>(CatalogoAlcuni));
+			Server.Instance.AddEventHandler("lprp:cardealer:cambiaVehCatalogo", new Action<Player, List<int>, string>(CambiaVeh));
 		}
 
 		private static void CatalogoAlcuni([FromSource] Player p, List<int> players)
 		{
 			p.TriggerEvent("lprp:cardealer:catalogoAlcuni", true, players);
 			players.ForEach(x => Funzioni.GetPlayerFromId(x).TriggerEvent("lprp:cardealer:catalogoAlcuni", false, players));
+		}
+		private static void CambiaVeh([FromSource] Player p, List<int> players, string vehName)
+		{
+			p.TriggerEvent("lprp:cardealer:catalogoAlcuni", true, vehName);
+			players.ForEach(x => Funzioni.GetPlayerFromId(x).TriggerEvent("lprp:cardealer:catalogoAlcuni", false, vehName));
 		}
 	}
 }
