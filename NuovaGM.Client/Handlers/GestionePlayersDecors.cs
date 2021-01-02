93b1a887778cf6cbbf122fc54f6444442a7f69d8
manups4e <manups4e@gmail.com>
2021-01-02 21:24:20 +0100

    fix errore sui decors al riavvio della GameMode

--- NuovaGM.Client/Handlers/GestionePlayersDecors.cs
+++ NuovaGM.Client/Handlers/GestionePlayersDecors.cs
@@ -15,44 +15,47 @@ namespace TheLastPlanet.Client
 			Ped playerPed = Game.PlayerPed;
 			foreach (var player in Client.Instance.GetPlayers)
 			{
-				if (player.Character.HasDecor("PlayerStanziato"))
+				if (player.GetPlayerData() != null)
 				{
-					if (player.Character.GetDecor<bool>("PlayerStanziato") || player.GetPlayerData().Istanza.Stanziato && player != Game.Player)
+					if (player.Character.HasDecor("PlayerStanziato"))
 					{
-						if (player.Character.HasDecor("PlayerStanziatoInIstanza") || player.GetPlayerData().Istanza.Instance != null)
+						if (player.Character.GetDecor<bool>("PlayerStanziato") || player.GetPlayerData().Istanza.Stanziato && player != Game.Player)
 						{
-							if (player.Character.GetDecor<int>("PlayerStanziatoInIstanza") != 0 || player.GetPlayerData().Istanza.ServerId != 0 || playerPed.GetDecor<int>("PlayerStanziatoInIstanza") != 0 || Game.Player.GetPlayerData().Istanza.ServerId != 0)
+							if (player.Character.HasDecor("PlayerStanziatoInIstanza") || player.GetPlayerData().Istanza.Instance != null)
 							{
-								if (player.Character.GetDecor<int>("PlayerStanziatoInIstanza") != Game.Player.ServerId && playerPed.GetDecor<int>("PlayerStanziatoInIstanza") != player.ServerId || player.GetPlayerData().Istanza.ServerId != Game.Player.ServerId && Game.Player.GetPlayerData().Istanza.ServerId != player.ServerId)
+								if (player.Character.GetDecor<int>("PlayerStanziatoInIstanza") != 0 || player.GetPlayerData().Istanza.ServerId != 0 || playerPed.GetDecor<int>("PlayerStanziatoInIstanza") != 0 || Game.Player.GetPlayerData().Istanza.ServerId != 0)
 								{
-									if (!NetworkIsPlayerConcealed(player.Handle))
-										NetworkConcealPlayer(player.Handle, true, true);
+									if (player.Character.GetDecor<int>("PlayerStanziatoInIstanza") != Game.Player.ServerId && playerPed.GetDecor<int>("PlayerStanziatoInIstanza") != player.ServerId || player.GetPlayerData().Istanza.ServerId != Game.Player.ServerId && Game.Player.GetPlayerData().Istanza.ServerId != player.ServerId)
+									{
+										if (!NetworkIsPlayerConcealed(player.Handle))
+											NetworkConcealPlayer(player.Handle, true, true);
+									}
+									else
+									{
+										if (player.Character.GetDecor<int>("PlayerStanziatoInIstanza") == Game.Player.ServerId || playerPed.GetDecor<int>("PlayerStanziatoInIstanza") == player.ServerId || player.GetPlayerData().Istanza.ServerId == Game.Player.ServerId || Game.Player.ServerId == player.GetPlayerData().Istanza.ServerId)
+										{
+											if (NetworkIsPlayerConcealed(player.Handle))
+												NetworkConcealPlayer(player.Handle, false, false);
+										}
+									}
 								}
 								else
 								{
-									if (player.Character.GetDecor<int>("PlayerStanziatoInIstanza") == Game.Player.ServerId || playerPed.GetDecor<int>("PlayerStanziatoInIstanza") == player.ServerId || player.GetPlayerData().Istanza.ServerId == Game.Player.ServerId || Game.Player.ServerId == player.GetPlayerData().Istanza.ServerId)
-									{
-										if (NetworkIsPlayerConcealed(player.Handle))
-											NetworkConcealPlayer(player.Handle, false, false);
-									}
+									if (NetworkIsPlayerConcealed(player.Handle))
+										NetworkConcealPlayer(player.Handle, false, false);
 								}
 							}
 							else
 							{
-								if (NetworkIsPlayerConcealed(player.Handle))
-									NetworkConcealPlayer(player.Handle, false, false);
+								if (!NetworkIsPlayerConcealed(player.Handle))
+									NetworkConcealPlayer(player.Handle, true, true);
 							}
 						}
 						else
 						{
-							if (!NetworkIsPlayerConcealed(player.Handle))
-								NetworkConcealPlayer(player.Handle, true, true);
+							if (NetworkIsPlayerConcealed(player.Handle))
+								NetworkConcealPlayer(player.Handle, false, false);
 						}
-					}
-					else
-					{
-						if (NetworkIsPlayerConcealed(player.Handle))
-							NetworkConcealPlayer(player.Handle, false, false);
 					}
 				}
 				if (player.Character.HasDecor("PlayerInPausa"))
