cc234ff657ab5971a44e4fb2304067bb2655e5f0
manups4e <manups4e@gmail.com>
2021-02-11 16:03:44 +0100

    fix classe non trovata

--- NuovaGM.Client/ClassCollector.cs
+++ NuovaGM.Client/ClassCollector.cs
@@ -66,7 +66,7 @@ namespace TheLastPlanet.Client
 			Interactions.PickupsClient.Init();
 			Interactions.OggettiGenerici.Init();
 			Interactions.PrimaPersonaObbligatoria.Init();
-			DamageEvents.Init();
+			InternalGameEvents.Init();
 
 			Sport.Yoga.Init();
 
