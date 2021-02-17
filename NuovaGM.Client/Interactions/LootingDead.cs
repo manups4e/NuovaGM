08530c3fd8e8dab2c28b20fa9d7b5d2f83b320fb
manups4e <manups4e@gmail.com>
2021-02-17 20:54:54 +0100

    iniziato Looting Dead

---
+++ NuovaGM.Client/Interactions/LootingDead.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLastPlanet.Client.Interactions
+{
+	static class LootingDead
+	{
+		public static void Init()
+		{
+
+		}
+	}
+}
