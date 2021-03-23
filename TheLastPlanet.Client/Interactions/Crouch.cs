5842d76dd21a54649fd750f3b9143ac6ff7c3cf4
manups4e <manups4e@gmail.com>
2021-03-23 10:25:53 +0100

    fix crouch

--- TheLastPlanet.Client/Interactions/Crouch.cs
+++ TheLastPlanet.Client/Interactions/Crouch.cs
@@ -1,7 +1,26 @@
-﻿namespace TheLastPlanet.Client.Interactions
+﻿using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using TheLastPlanet.Client.Handlers;
+
+namespace TheLastPlanet.Client.Interactions
 {
-	public class Crouch
+	static class Crouch
 	{
-		
+		public static bool IsCrouching { get; set; }
+
+		public static void Init()
+		{
+			InputHandler.ListaInput.Add(new InputController(Control.Duck, action: new Action<Ped>(Crouching)));
+		}
+
+		public static void Crouching(Ped me)
+		{
+			API.DisableControlAction(0, 36, true);
+			API.SetPedStealthMovement(me.Handle, false, "");
+			if (IsCrouching) me.MovementAnimationSet = "move_ped_crouched";
+			else API.ResetPedMovementClipset(me.Handle, 0);
+		}
 	}
 }
