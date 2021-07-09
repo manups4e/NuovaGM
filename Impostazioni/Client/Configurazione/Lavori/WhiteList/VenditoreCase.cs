847e520da9cbd60236749ce3a301bba47012dfaf
manups4e <manups4e@gmail.com>
2021-07-09 11:01:06 +0200

    Agg. Config client

--- Impostazioni/Client/Configurazione/Lavori/WhiteList/VenditoreCase.cs
+++ Impostazioni/Client/Configurazione/Lavori/WhiteList/VenditoreCase.cs
@@ -6,62 +6,8 @@ namespace Impostazioni.Client.Configurazione.Lavori.WhiteList
 {
     public class ConfigVenditoriCase
     {
-        public ConfigurazioneVendCase Config = new ConfigurazioneVendCase()
-        {
-            BossActions = new Position(-32.0f, -1114.2f, 25.4f),
-            Ingresso = new Position(-199.151f, -575.000f, 39.489f),
-            Uscita = new Position(-141.226f, -614.166f, 167.820f),
-            Dentro = new Position(-140.969f, -616.785f, 167.820f),
-            Fuori = new Position(-202.238f, -578.193f, 39.500f),
-            Actions = new Position(-124.786f, -641.486f, 167.820f)
-        };
+        public ConfigurazioneVendCase Config{ get; set; }
 
-        public Dictionary<string, JobGrade> Gradi = new Dictionary<string, JobGrade>()
-        {
-            ["Venditore"] = new JobGrade()
-            {
-                Id = 0,
-                Stipendio = 0,
-                Vestiti = new()
-                {
-                    Maschio = new()
-                    {
-                        Abiti = new(),
-                        TextureVestiti = new(),
-                        Accessori = new(),
-                        TexturesAccessori = new(),
-                    },
-                    Femmina = new()
-                    {
-                        Abiti = new(),
-                        TextureVestiti = new(),
-                        Accessori = new(),
-                        TexturesAccessori = new(),
-                    }
-                }
-            },
-            ["Direttore"] = new JobGrade()
-            {
-                Id = 1,
-                Stipendio = 0,
-                Vestiti = new()
-                {
-                    Maschio = new()
-                    {
-                        Abiti = new(),
-                        TextureVestiti = new(),
-                        Accessori = new(),
-                        TexturesAccessori = new(),
-                    },
-                    Femmina = new()
-                    {
-                        Abiti = new(),
-                        TextureVestiti = new(),
-                        Accessori = new(),
-                        TexturesAccessori = new(),
-                    }
-                }
-            },
-        };
+        public Dictionary<string, JobGrade> Gradi{ get; set; }
     }
 }
