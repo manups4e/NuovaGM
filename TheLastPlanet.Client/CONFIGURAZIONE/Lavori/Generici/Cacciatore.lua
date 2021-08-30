c5b27e4c3d1030c494b261eaf425bae5842784e0
manups4e <manups4e@gmail.com>
2021-08-30 10:24:39 +0200

    rimossa vecchia configurazione in lua

--- TheLastPlanet.Client/CONFIGURAZIONE/Lavori/Generici/Cacciatore.lua
+++
@@ -1,6 +0,0 @@
-Config.Client.Lavori.Generici.Cacciatore = {
-    inizioCaccia = vector3(-1490.436, 4981.299, 63.357),
-    zonaDiCaccia = vector3(-1128.312, 4924.958, 220.224),
-    limiteArea = 700.5,
-    UccidiAltriPlayers = false,
-}
--- TheLastPlanet.Client/CONFIGURAZIONE/Veicoli/barcheAff.lua
+++
@@ -1,7 +0,0 @@
-Config.Client.Veicoli.barcheAff = {
-    sottomarini = {},
-    barche = {
-        {name = "Dinghy", price = 1750, description = "", model = "dinghy"},
-    },
-    lusso = {},
-}
