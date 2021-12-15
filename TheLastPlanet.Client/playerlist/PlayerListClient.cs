ada76b00b3a154d1275aa76858bf82ed3ad35d4c
Leonardo Emanuele <manups4e@gmail.com>
2021-12-15 10:11:13 +0100

    PlayerList ora per tutte le modalità e filtrata

--- TheLastPlanet.Client/playerlist/PlayerListClient.cs
+++ TheLastPlanet.Client/playerlist/PlayerListClient.cs
@@ -9,72 +9,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using static CitizenFX.Core.Native.API;
 using TheLastPlanet.Client.MODALITA.ROLEPLAY.Banking;
+using TheLastPlanet.Client.Cache;
+using ScaleformUI;
+using TheLastPlanet.Shared;
 
 namespace TheLastPlanet.Client.ListaPlayers
 {
 	internal static class FivemPlayerlist
 	{
-		private static int maxClients = -1;
-		private static bool ScaleSetup = false;
-		private static int currentPage = 0;
-		private static Scaleform scale;
-		private static int maxPages = 1;
-
-		public struct PlayerRowConfig
-		{
-			public string crewName;
-			public int jobPoints;
-			public bool showJobPointsIcon;
-		}
-
-		private static Dictionary<int, PlayerRowConfig> playerConfigs = new Dictionary<int, PlayerRowConfig>();
-
-		/// <summary>
-		/// Constructor
-		/// </summary>
+		static int maxPlayers = 0;
 		public static void Init()
 		{
-			BaseScript.TriggerServerEvent("lprp:fs:getMaxPlayers");
-			Client.Instance.AddTick(ShowScoreboard);
 			Client.Instance.AddTick(DisplayController);
-			Client.Instance.AddTick(BackupTimer);
-			Client.Instance.AddEventHandler("lprp:fs:setMaxPlayers", new Action<int>(SetMaxPlayers));
-			Client.Instance.AddEventHandler("lprp:fs:setPlayerConfig", new Action<int, string, int, bool>(SetPlayerConfig));
 		}
 
-		/// <summary>
-		/// Set the config for the specified player.
-		/// </summary>
-		/// <param name="playerServerId"></param>
-		/// <param name="crewname"></param>
-		/// <param name="jobpoints"></param>
-		/// <param name="showJPicon"></param>
-		private static async void SetPlayerConfig(int playerServerId, string crewname, int jobpoints, bool showJPicon)
+		public static void MostraMoney()
 		{
-			PlayerRowConfig cfg = new PlayerRowConfig() { crewName = crewname ?? "", jobPoints = jobpoints, showJobPointsIcon = showJPicon };
-			playerConfigs[playerServerId] = cfg;
-			if (currentPage > -1) await LoadScale();
+			N_0x170f541e1cadd1de(true);
+			SetMultiplayerWalletCash();
+			SetMultiplayerBankCash();
+			N_0x170f541e1cadd1de(false);
 		}
 
-		/// <summary>
-		/// Used to close the page if the regular timer fails to close it for some odd reason.
-		/// </summary>
-		/// <returns></returns>
-		private static async Task BackupTimer()
+		public static void NascondiMoney()
 		{
-			int timer = GetGameTimer();
-			int oldPage = currentPage;
-			while (GetGameTimer() - timer < 8000 && currentPage > 0 && currentPage == oldPage) await BaseScript.Delay(0);
-			if (oldPage == currentPage) currentPage = 0;
+			RemoveMultiplayerWalletCash();
+			RemoveMultiplayerBankCash();
 		}
 
-		/// <summary>
-		/// Updates the max pages to disaplay based on the player count.
-		/// </summary>
-		private static void UpdateMaxPages()
-		{
-			maxPages = (int)Math.Ceiling((double)Client.Instance.GetPlayers.ToList().Count() / 16.0);
-		}
+		//TODO: QUANDO ENTRA/ESCE AGGIORNA LISTA.. E LA RICHIESTA è FATTA SOLO AL LOGIN
 
 		/// <summary>
 		/// Manages the display and page setup of the playerlist.
@@ -82,271 +45,155 @@ namespace TheLastPlanet.Client.ListaPlayers
 		/// <returns></returns>
 		private static async Task DisplayController()
 		{
-			if (Input.IsControlJustPressed(Control.MultiplayerInfo) && !HUD.MenuPool.IsAnyMenuOpen && !BankingClient.InterfacciaAperta && !IsPedRunningMobilePhoneTask(PlayerPedId()))
+			if (Input.IsControlJustPressed(Control.MultiplayerInfo) && !HUD.MenuPool.IsAnyMenuOpen && !IsPedRunningMobilePhoneTask(PlayerPedId()))
 			{
-				if (!Screen.Hud.IsComponentActive(HudComponent.MpCash)) BankingClient.MostraMoney();
-				UpdateMaxPages();
+				NativeUIScaleform.PlayerListInstance.PlayerRows.Clear();
+				var num = await Client.Instance.Events.Get<int>("tlg:fs:getMaxPlayers", PlayerCache.ModalitàAttuale);
+				List<PlayerSlot> list = await Client.Instance.Events.Get<List<PlayerSlot>>("tlg:fs:getPlayers", PlayerCache.ModalitàAttuale);
+                if (PlayerCache.ModalitàAttuale == ModalitaServer.Roleplay)
+                {
+					if (BankingClient.InterfacciaAperta)
+						return;
+				}
 
-				if (ScaleSetup)
+				foreach (var p in list)
+                {
+					Ped ped = p.ServerId == PlayerCache.MyPlayer.Handle ? PlayerCache.MyPlayer.Ped : Funzioni.GetClientIdFromServerId(p.ServerId).Ped;
+					var mug = await Funzioni.GetPedMugshotAsync(ped);
+					if (NativeUIScaleform.PlayerListInstance.PlayerRows.Any(x => x.ServerId == p.ServerId)) continue;
+					var row = new PlayerRow()
+					{
+						Color = p.Color,
+						CrewLabelText = p.CrewLabelText,
+						FriendType = p.FriendType,
+						IconOverlayText = p.IconOverlayText,
+						JobPointsDisplayType = (ScoreDisplayType)p.JobPointsDisplayType,
+						JobPointsText = p.JobPointsText,
+						Name = p.Name,
+						RightIcon = (ScoreRightIconType)p.RightIcon,
+						RightText = p.RightText,
+						ServerId = p.ServerId,
+						TextureString = mug.Item2
+					};
+					NativeUIScaleform.PlayerListInstance.AddRow(row);
+					UnregisterPedheadshot(mug.Item1);
+				}
+				NativeUIScaleform.PlayerListInstance.SetTitle($"Modalità {PlayerCache.ModalitàAttuale} (online {num})", $"{(NativeUIScaleform.PlayerListInstance.CurrentPage+1)} / {NativeUIScaleform.PlayerListInstance.MaxPages}", 2);
+				NativeUIScaleform.PlayerListInstance.CurrentPage++;
+			}
+			if (NativeUIScaleform.PlayerListInstance.Enabled)
+			{
+				if (!Screen.Hud.IsComponentActive(HudComponent.MpCash)) MostraMoney();
+				if (NativeUIScaleform.PlayerListInstance.PlayerRows.Count > 0)
 				{
-					currentPage++;
-
-					if (currentPage > maxPages)
+					foreach (var p in NativeUIScaleform.PlayerListInstance.PlayerRows)
 					{
-						currentPage = 0;
-						if (Screen.Hud.IsComponentActive(HudComponent.MpCash)) BankingClient.NascondiMoney();
-					}
-
-					await LoadScale();
-					int timer = GetGameTimer();
-					bool nextPage = false;
-
-					while (GetGameTimer() - timer < 5000)
-					{
-						await BaseScript.Delay(1);
-
-						if (Input.IsControlJustPressed(Control.MultiplayerInfo))
-						{
-							nextPage = true;
-
-							break;
-						}
-					}
-
-					if (nextPage)
-					{
-						UpdateMaxPages();
-
-						if (currentPage < maxPages)
-						{
-							currentPage++;
-							await LoadScale();
-						}
+						var player = GetPlayerFromServerId(p.ServerId);
+						var index = NativeUIScaleform.PlayerListInstance.PlayerRows.IndexOf(p);
+						if (NetworkIsPlayerTalking(player) || MumbleIsPlayerTalking(player))
+							NativeUIScaleform.PlayerListInstance.SetIcon(index, ScoreRightIconType.ACTIVE_HEADSET, "");
 						else
-						{
-							currentPage = 0;
-							if (Screen.Hud.IsComponentActive(HudComponent.MpCash)) BankingClient.NascondiMoney();
-						}
-					}
-					else
-					{
-						currentPage = 0;
-						if (Screen.Hud.IsComponentActive(HudComponent.MpCash)) BankingClient.NascondiMoney();
+							NativeUIScaleform.PlayerListInstance.SetIcon(index, p.RightIcon, p.RightText);
 					}
 				}
 			}
-		}
-
-		/// <summary>
-		/// Updates the max Players (triggered from server event)
-		/// </summary>
-		/// <param name="count"></param>
-		private static void SetMaxPlayers(int count)
-		{
-			maxClients = count;
-		}
-
-		/// <summary>
-		/// Shows the scoreboard.
-		/// </summary>
-		/// <returns></returns>
-		private static async Task ShowScoreboard()
-		{
-			if (maxClients != -1 && Main.spawned)
+			else
 			{
-				if (!ScaleSetup)
+				if (Screen.Hud.IsComponentActive(HudComponent.MpCash)) NascondiMoney();
+			}
+		}
+		/*
+				private static async Task LoadScale()
 				{
-					await LoadScale();
-					ScaleSetup = true;
-				}
-
-				if (currentPage > 0)
-				{
-					float safezone = GetSafeZoneSize();
-					float change = (safezone - 0.89f) / 0.11f;
-					float x = 50f;
-					x -= change * 78f;
-					float y = 50f;
-					y -= change * 50f;
-					float width = 350f;
-					float height = 490f;
 					if (scale != null)
-						if (scale.IsLoaded)
-							DrawScaleformMovie(scale.Handle, 0.122f, 0.3f, 0.28f, 0.6f, 255, 255, 255, 255, 0);
-						//scale.Render2DScreenSpace(new System.Drawing.PointF(x, y), new System.Drawing.PointF(width, height));
+					{
+						for (int i = 0; i < maxClients * 2; i++) scale.CallFunction("SET_DATA_SLOT_EMPTY", i);
+						scale.Dispose();
+					}
+
+					scale = null;
+					while (!HasScaleformMovieLoaded(RequestScaleformMovie("MP_MM_CARD_FREEMODE"))) await BaseScript.Delay(0);
+					scale = new Scaleform("MP_MM_CARD_FREEMODE");
+					string titleIcon = "2";
+					string titleLeftText = "The Last Galaxy - " + PlayerCache.ModalitàAttuale.ToString();
+					string titleRightText = $"Players {NetworkGetNumConnectedPlayers()}/{maxClients}";
+					scale.CallFunction("SET_TITLE", titleLeftText, titleRightText, titleIcon);
+					await UpdateScale();
+					scale.CallFunction("DISPLAY_VIEW");
 				}
-			}
-		}
 
-		/// <summary>
-		/// Loads the scaleform.
-		/// </summary>
-		/// <returns></returns>
-		private static async Task LoadScale()
-		{
-			if (scale != null)
-			{
-				for (int i = 0; i < maxClients * 2; i++) scale.CallFunction("SET_DATA_SLOT_EMPTY", i);
-				scale.Dispose();
-			}
 
-			scale = null;
-			while (!HasScaleformMovieLoaded(RequestScaleformMovie("MP_MM_CARD_FREEMODE"))) await BaseScript.Delay(0);
-			scale = new Scaleform("MP_MM_CARD_FREEMODE");
-			string titleIcon = "2";
-			string titleLeftText = Client.Impostazioni.RolePlay.Main.NomeServer;
-			string titleRightText = $"Players {NetworkGetNumConnectedPlayers()}/{maxClients}";
-			scale.CallFunction("SET_TITLE", titleLeftText, titleRightText, titleIcon);
-			await UpdateScale();
-			scale.CallFunction("DISPLAY_VIEW");
-		}
-
-		/// <summary>
-		/// Struct used for the player info row options.
-		/// </summary>
-		private struct PlayerRow
-		{
-			public int serverId;
-			public string name;
-			public string rightText;
-			public int color;
-			public string iconOverlayText;
-			public string jobPointsText;
-			public string crewLabelText;
-
-			public enum DisplayType
-			{
-				NUMBER_ONLY = 0,
-				ICON = 1,
-				NONE = 2
-			};
-
-			public DisplayType jobPointsDisplayType;
-
-			public enum RightIconType
-			{
-				NONE = 0,
-				INACTIVE_HEADSET = 48,
-				MUTED_HEADSET = 49,
-				ACTIVE_HEADSET = 47,
-				RANK_FREEMODE = 65,
-				KICK = 64,
-				LOBBY_DRIVER = 79,
-				LOBBY_CODRIVER = 80,
-				SPECTATOR = 66,
-				BOUNTY = 115,
-				DEAD = 116,
-				DPAD_GANG_CEO = 121,
-				DPAD_GANG_BIKER = 122,
-				DPAD_DOWN_TARGET = 123
-			};
-
-			public int rightIcon;
-			public string textureString;
-			public char friendType;
-		}
-
-		/// <summary>
-		/// Returns the ped headshot string used for the image of the ped for each row.
-		/// </summary>
-		/// <param name="ped"></param>
-		/// <returns></returns>
-		private static async Task<string> GetHeadshotImage(int ped)
-		{
-			int headshotHandle = RegisterPedheadshot(ped);
-			while (!IsPedheadshotReady(headshotHandle)) await BaseScript.Delay(0);
-
-			return GetPedheadshotTxdString(headshotHandle) ?? "";
-		}
-
-		/// <summary>
-		/// Updates the scaleform settings.
-		/// </summary>
-		/// <returns></returns>
-		private static async Task UpdateScale()
-		{
-			List<PlayerRow> rows = new List<PlayerRow>();
-			for (int x = 0; x < 128; x++) // cleaning up in case of a reload, this frees up all ped headshot handles :)
-				UnregisterPedheadshot(x);
-			int amount = 0;
-
-			foreach (Player p in Client.Instance.GetPlayers.ToList())
-			{
-				if (IsRowSupposedToShow(amount))
+				/// <summary>
+				/// Updates the scaleform settings.
+				/// </summary>
+				/// <returns></returns>
+				private static async Task UpdateScale()
 				{
-					if (playerConfigs.ContainsKey(p.ServerId))
+					List<PlayerRow> rows = new List<PlayerRow>();
+					for (int x = 0; x < 128; x++) // cleaning up in case of a reload, this frees up all ped headshot handles :)
+						UnregisterPedheadshot(x);
+					int amount = 0;
+
+					var players = Client.Instance.Events.Get<List<PlayerRow>>("tlg:fs:getPlayersRows");
+
+					foreach (Player p in Client.Instance.GetPlayers.ToList())
 					{
-						PlayerRow row = new()
+						if (IsRowSupposedToShow(amount))
 						{
-							color = 111,
-							crewLabelText = playerConfigs[p.ServerId].crewName,
-							friendType = ' ',
-							iconOverlayText = "",
-							jobPointsDisplayType = playerConfigs[p.ServerId].showJobPointsIcon ? PlayerRow.DisplayType.ICON : playerConfigs[p.ServerId].jobPoints >= 0 ? PlayerRow.DisplayType.NUMBER_ONLY : PlayerRow.DisplayType.NONE,
-							jobPointsText = playerConfigs[p.ServerId].jobPoints >= 0 ? playerConfigs[p.ServerId].jobPoints.ToString() : "",
-							name = p.Name.Replace("<", "").Replace(">", "").Replace("^", "").Replace("~", "").Trim(),
-							rightIcon = (int)PlayerRow.RightIconType.RANK_FREEMODE,
-							rightText = $"{p.ServerId}",
-							serverId = p.ServerId
-						};
-						row.textureString = (await Funzioni.GetPedMugshotAsync(p.Character)).Item2;
-						rows.Add(row);
+							if (playerConfigs.ContainsKey(p.ServerId))
+							{
+								PlayerRow row = new()
+								{
+									color = 111,
+									crewLabelText = playerConfigs[p.ServerId].crewName,
+									friendType = ' ',
+									iconOverlayText = "",
+									jobPointsDisplayType = playerConfigs[p.ServerId].showJobPointsIcon ? PlayerRow.DisplayType.ICON : playerConfigs[p.ServerId].jobPoints >= 0 ? PlayerRow.DisplayType.NUMBER_ONLY : PlayerRow.DisplayType.NONE,
+									jobPointsText = playerConfigs[p.ServerId].jobPoints >= 0 ? playerConfigs[p.ServerId].jobPoints.ToString() : "",
+									name = p.Name.Replace("<", "").Replace(">", "").Replace("^", "").Replace("~", "").Trim(),
+									rightIcon = (int)PlayerRow.RightIconType.RANK_FREEMODE,
+									rightText = $"{p.ServerId}",
+									serverId = p.ServerId
+								};
+								row.textureString = (await Funzioni.GetPedMugshotAsync(p.Character)).Item2;
+								rows.Add(row);
+							}
+							else
+							{
+								PlayerRow row = new()
+								{
+									color = 111,
+									crewLabelText = "",
+									friendType = ' ',
+									iconOverlayText = "",
+									jobPointsDisplayType = PlayerRow.DisplayType.NUMBER_ONLY,
+									jobPointsText = "",
+									name = p.Name.Replace("<", "").Replace(">", "").Replace("^", "").Replace("~", "").Trim(),
+									rightIcon = (int)PlayerRow.RightIconType.RANK_FREEMODE,
+									rightText = $"{p.ServerId}",
+									serverId = p.ServerId
+								};
+								row.textureString = (await Funzioni.GetPedMugshotAsync(p.Character)).Item2;
+								rows.Add(row);
+							}
+						}
+
+						amount++;
 					}
-					else
+
+					rows.Sort((row1, row2) => row1.serverId.CompareTo(row2.serverId));
+					for (int i = 0; i < maxClients * 2; i++) scale.CallFunction("SET_DATA_SLOT_EMPTY", i);
+					int index = 0;
+
+					foreach (PlayerRow row in rows)
 					{
-						PlayerRow row = new()
-						{
-							color = 111,
-							crewLabelText = "",
-							friendType = ' ',
-							iconOverlayText = "",
-							jobPointsDisplayType = PlayerRow.DisplayType.NUMBER_ONLY,
-							jobPointsText = "",
-							name = p.Name.Replace("<", "").Replace(">", "").Replace("^", "").Replace("~", "").Trim(),
-							rightIcon = (int)PlayerRow.RightIconType.RANK_FREEMODE,
-							rightText = $"{p.ServerId}",
-							serverId = p.ServerId
-						};
-						row.textureString = (await Funzioni.GetPedMugshotAsync(p.Character)).Item2;
-						rows.Add(row);
+						if (row.crewLabelText != "")
+							scale.CallFunction("SET_DATA_SLOT", index, row.rightText, row.name, row.color, row.rightIcon, row.iconOverlayText, row.jobPointsText, $"..+{row.crewLabelText}", (int)row.jobPointsDisplayType, row.textureString, row.textureString, row.friendType);
+						else
+							scale.CallFunction("SET_DATA_SLOT", index, row.rightText, row.name, row.color, row.rightIcon, row.iconOverlayText, row.jobPointsText, "", (int)row.jobPointsDisplayType, row.textureString, row.textureString, row.friendType);
+						index++;
 					}
 				}
-
-				amount++;
-			}
-
-			rows.Sort((row1, row2) => row1.serverId.CompareTo(row2.serverId));
-			for (int i = 0; i < maxClients * 2; i++) scale.CallFunction("SET_DATA_SLOT_EMPTY", i);
-			int index = 0;
-
-			foreach (PlayerRow row in rows)
-			{
-				if (row.crewLabelText != "")
-					scale.CallFunction("SET_DATA_SLOT", index, row.rightText, row.name, row.color, row.rightIcon, row.iconOverlayText, row.jobPointsText, $"..+{row.crewLabelText}", (int)row.jobPointsDisplayType, row.textureString, row.textureString, row.friendType);
-				else
-					scale.CallFunction("SET_DATA_SLOT", index, row.rightText, row.name, row.color, row.rightIcon, row.iconOverlayText, row.jobPointsText, "", (int)row.jobPointsDisplayType, row.textureString, row.textureString, row.friendType);
-				index++;
-			}
-		}
-
-		/// <summary>
-		/// Used to check if the row from the loop is supposed to be displayed based on the current page view.
-		/// </summary>
-		/// <param name="row"></param>
-		/// <returns></returns>
-		private static bool IsRowSupposedToShow(int row)
-		{
-			if (currentPage > 0)
-			{
-				int max = currentPage * 16;
-				int min = currentPage * 16 - 16;
-
-				if (row >= min && row < max) return true;
-
-				return false;
-			}
-
-			return false;
-		}
+		*/
 	}
 }
--- TheLastPlanet.Server/playerlist/PlayerListServer.cs
+++ TheLastPlanet.Server/playerlist/PlayerListServer.cs
@@ -4,38 +4,93 @@ using System;
 using System.Collections.Concurrent;
 using System.Linq;
 using static CitizenFX.Core.Native.API;
+using TheLastPlanet.Shared.Internal.Events;
+using TheLastPlanet.Server.Core.Buckets;
+using TheLastPlanet.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FivemPlayerlistServer
 {
-	public static class FPLServer
+	public static class PlayerListServer
 	{
 
 		private static ConcurrentDictionary<int, dynamic[]> list = new ConcurrentDictionary<int, dynamic[]>();
 		public static void Init()
 		{
-			Server.Instance.AddEventHandler("lprp:fs:getMaxPlayers", new Action<Player>(ReturnMaxPlayers));
+		
+			Server.Instance.Events.Mount("tlg:fs:getMaxPlayers", new Func<ClientId, ModalitaServer, Task<int>>(ReturnMaxPlayers));
+			Server.Instance.Events.Mount("tlg:fs:getPlayers", new Func<ClientId, ModalitaServer, Task<List<PlayerSlot>>>(ReturnPlayers));
 			Server.Instance.RegisterExport("setPlayerRowConfig", new Action<string, string, string, string>(SetPlayerConfig2));
-			Server.Instance.AddEventHandler("lprp:fs:setPlayerRowConfig", new Action<int, string, int, bool>(SetPlayerConfig));
+			Server.Instance.Events.Mount("tlg:fs:setPlayerRowConfig", new Action<int, string, int, bool>(SetPlayerConfig));
 		}
 
-		private static async void ReturnMaxPlayers([FromSource] Player source)
+		private static async Task<int> ReturnMaxPlayers(ClientId source, ModalitaServer mod)
 		{
 			await BaseScript.Delay(0);
-			source.TriggerEvent("lprp:fs:setMaxPlayers", int.Parse(GetConvar("sv_maxClients", "30").ToString()));
-
-			foreach (Player p in Server.Instance.GetPlayers.ToList())
-				if (list.ContainsKey(int.Parse(p.Handle)))
-				{
-					dynamic[] listItem = list[int.Parse(p.Handle)];
-					dynamic p1 = listItem[0];
-					dynamic p2 = listItem[1];
-					dynamic p3 = listItem[2];
-					dynamic p4 = listItem[3];
-					source.TriggerEvent("lprp:fs:setPlayerRowConfig", p1, p2, p3, p4);
-					await BaseScript.Delay(1);
-				}
+            switch (mod)
+            {
+				case ModalitaServer.Lobby:
+					return Server.Instance.GetPlayers.Count();
+				case ModalitaServer.FreeRoam:
+					return BucketsHandler.FreeRoam.GetTotalPlayers();
+				case ModalitaServer.Roleplay:
+					return BucketsHandler.RolePlay.GetTotalPlayers();
+				case ModalitaServer.Gare:
+					return BucketsHandler.Gare.GetTotalPlayers();
+				case ModalitaServer.Minigiochi:
+					return BucketsHandler.Minigiochi.GetTotalPlayers();
+				default:
+					return 0;
+			}
 		}
+		private static async Task<List<PlayerSlot>> ReturnPlayers(ClientId source, ModalitaServer mod)
+        {
+			List<PlayerSlot> result = new();
+			foreach (var client in Server.Instance.Clients)
+			{
+                PlayerSlot slot = new()
+                {
+                    CrewLabelText = String.Empty,
+                    FriendType = ' ',
+                    IconOverlayText = String.Empty,
+                    JobPointsDisplayType = SlotScoreDisplayType.NONE,
+                    JobPointsText = String.Empty,
+                    Name = client.Player.Name.Replace("<", "").Replace(">", "").Replace("^", "").Replace("~", "").Trim(),
+                    ServerId = client.Handle
+                };
 
+                switch (client.User.Status.PlayerStates.Modalita)
+                {
+					case ModalitaServer.Lobby:
+						slot.RightIcon = SlotScoreRightIconType.NONE;
+						slot.RightText = $"Lobby";
+						slot.Color = 3;
+					break;
+					case ModalitaServer.FreeRoam:
+						slot.RightIcon = SlotScoreRightIconType.RANK_FREEMODE;
+						slot.RightText = $"{client.User.FreeRoamChar.Level}";
+						slot.Color = 21;
+						break;
+					case ModalitaServer.Roleplay:
+						slot.RightIcon = SlotScoreRightIconType.NONE;
+						slot.RightText = $"ID {client.Handle}";
+						slot.Color = 116;
+						break;
+						//case ModalitaServer.Gare:
+						//case ModalitaServer.Minigiochi:
+				}
+				result.Add(slot);
+            }
+
+            return mod switch
+            {
+                ModalitaServer.Lobby => result,
+                ModalitaServer.FreeRoam => result.Where(x => x.Color == 21).ToList(),
+                ModalitaServer.Roleplay => result.Where(x => x.Color == 116).ToList(),
+                _ => result,
+            };
+        }
 		private static void SetPlayerConfig2(string playerServerId, string crewName, string jobPoints, string showJobPointsIcon)
 		{
 			SetPlayerConfig(int.Parse(playerServerId), crewName, int.Parse(jobPoints ?? "-1"), bool.Parse(showJobPointsIcon ?? "false"));
@@ -45,7 +100,7 @@ namespace FivemPlayerlistServer
 			if (playerServerId > 0)
 			{
 				list[playerServerId] = new dynamic[4] { playerServerId, crewName ?? "", jobPoints, showJobPointsIcon };
-				BaseScript.TriggerClientEvent("lprp:fs:setPlayerConfig", playerServerId, crewName ?? "", jobPoints,
+				BaseScript.TriggerClientEvent("tlg:fs:setPlayerConfig", playerServerId, crewName ?? "", jobPoints,
 					showJobPointsIcon);
 			}
 		}
