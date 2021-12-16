f33cd563c183e620b275050f136c2713587c25d7
Leonardo Emanuele <manups4e@gmail.com>
2021-12-16 10:51:46 +0100

    EventMessage bisogna fixarlo...

--- TheLastPlanet.Shared/EventSubsystem/Message/EventMessage.cs
+++ TheLastPlanet.Shared/EventSubsystem/Message/EventMessage.cs
@@ -10,10 +10,10 @@ namespace TheLastPlanet.Shared.Internal.Events.Message
     {
         public Snowflake Id { get; set; }
         public string? Signature { get; set; }
-        public string Endpoint { get; set; }
+        public string? Endpoint { get; set; }
         public EventFlowType Flow { get; set; }
         public IEnumerable<EventParameter> Parameters { get; set; }
-
+        public EventMessage() { }
         public EventMessage(string endpoint, EventFlowType flow, IEnumerable<EventParameter> parameters)
         {
             Id = Snowflake.Next();
@@ -21,7 +21,6 @@ namespace TheLastPlanet.Shared.Internal.Events.Message
             Flow = flow;
             Parameters = parameters;
         }
-
         public override string ToString() => Endpoint;
     }
 }
