dafe258d70d5a06b146a6f0db25d02ed26c70a5e
Leonardo Emanuele <manups4e@gmail.com>
2021-12-22 22:45:41 +0100

    fix binary helper

--- TheLastPlanet.Shared/Extensions/BinaryHelper.cs
+++ TheLastPlanet.Shared/Extensions/BinaryHelper.cs
@@ -7,26 +7,28 @@ namespace TheLastPlanet.Shared
 {
     public static class BinaryHelper
     {
-        private static readonly BinarySerialization _serialization = new BinarySerialization();
+        private static ISerialization _serialization { get; set; }
 
         public static byte[] ToBytes<T>(this T obj)
         {
-            using SerializationContext context = new("BinarySerializer", "Binary serialization", _serialization);
+            _serialization = new BinarySerialization();
+            using SerializationContext context = new(typeof(T).ToString(), null, _serialization);
             context.Serialize(obj);
             return context.GetData();
+        }
+        public static byte[] ToBytes(this string str)
+        {
+            _serialization = new BinarySerialization();
+            var arr = str.ToCharArray();
+            if (arr[2] != '-' && arr[5] != '-') return default;
+            return str.Split('-').Select(x => byte.Parse(x, NumberStyles.HexNumber)).ToArray();
         }
 
         public static T FromBytes<T>(this byte[] data)
         {
-            using SerializationContext context = new("BinarySerializer", "Binary deserialization", _serialization, data);
+            _serialization = new BinarySerialization();
+            using SerializationContext context = new(data.ToString(), null, _serialization, data);
             return context.Deserialize<T>();
-        }
-
-        public static byte[] ToBytes(this string str)
-        {
-            var arr = str.ToCharArray();
-            if (arr[2] != '-' && arr[5] != '-') return default;
-            return str.Split('-').Select(x => byte.Parse(x, NumberStyles.HexNumber)).ToArray();
         }
     }
 }
