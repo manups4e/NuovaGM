3a02bd8a56c9ded20db55fb313ca2f45a6e2f8fd
Leonardo Emanuele <manups4e@gmail.com>
2022-02-20 00:24:05 +0100

    Aggiunto disoccupato come lavoro con stipendio base

---
+++ Impostazioni.Shared/Roleplay/Lavori/Disoccupato.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Impostazioni.Shared.Roleplay.Lavori
+{
+    public class Disoccupato
+    {
+        public int Stipendio = 600;
+    }
+}
